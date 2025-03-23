0a4dd309f534cb5bd25abeb330f4f629e84910f2
gongqi.zhen <gongqi.zhen@gmail.com>
2025-03-23 15:03:54 +0900

    動画プレイヤー生成処理のパフォーマンス改善: - HlsJS の enableWorker を true に設定して、処理をワーカーにオフロード ※ メソッド名 "destory" は別途修正予定

--- workspaces/client/src/features/player/logics/create_player.ts
+++ workspaces/client/src/features/player/logics/create_player.ts
@@ -1,4 +1,4 @@
-import '@videojs/http-streaming';
+iimport '@videojs/http-streaming';
 import HlsJs from 'hls.js';
 import shaka from 'shaka-player';
 import videojs from 'video.js';
@@ -72,7 +72,7 @@ class HlsJSPlayerWrapper implements PlayerWrapper {
     volume: 0.25,
   });
   private _player = new HlsJs({
-    enableWorker: false,
+    enableWorker: true, // enableWorker を true に変更
     maxBufferLength: 50,
   });
   readonly playerType: PlayerType.HlsJS;
@@ -194,3 +194,4 @@ export const createPlayer = (playerType: PlayerType): PlayerWrapper => {
     }
   }
 };
+
